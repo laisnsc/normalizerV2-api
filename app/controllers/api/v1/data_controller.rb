2d4ecee69867426a65d12addb60745d78829fa89
Laís <lais.sdsn@gmail.com>
2025-04-02 15:27:55 -0300

    :recycle: refactor: Controllers

--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -2,18 +2,22 @@ module Api::V1
   class DataController < ApplicationController
     before_action :data_params
     def process_data
-      file_data = NormalizerService.new(params[:file], params[:order_ids],
-                                        params[:start_date], params[:end_date])
-      render json: file_data.process_data, status: :ok
+      file_data = DataServices::FileService.new(params[:file])
+      processed = file_data.call
+      if processed
+        render json: { message: "File processed successfully" }, status: :ok
+      else
+        render json: { error: processed[:error] }, status: :unprocessable_entity
+      end
 
     rescue StandardError => e
-      render json: { error: e.message }, status: :internal_server_error
+      render json: { error: e.message }, status: :unprocessable_entity
     end
 
     private
 
     def data_params
-      params.permit(:file, :order_ids, :start_date, :end_date)
+      params.permit(:file)
     end
   end
 end
--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,8 +1,18 @@
 module Api::V1
   class OrdersController < ApplicationController
+    before_action :order_params
     def index
-      orders = Order.all
+      # orders = Order.all
+      orders = DataServices::SerializerService.new(params[:order_ids],
+                                                   params[:start_date],
+                                                   params[:end_date]).call
       render json: orders
+    end
+
+    private
+
+    def order_params
+      params.permit(:order_ids, :start_date, :end_date)
     end
   end
 end
